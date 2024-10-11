e4a4a2535579ce1c8ea5eaf3bb89065a38fa2de2
JZwlth <wkjazy87@gmail.com>
2024-10-10 20:21:27 -0700

    update

--- main.cpp
+++ main.cpp
@@ -60,28 +60,26 @@ public:
 };
 
 int main() {
-cout << fixed << setprecision(2);
-//creating pointer to first chair object
-Chair *chairPtr = new Chair;
-chairPtr->setLegs(4);
-chairPtr->setPrices(121.21, 232.32, 414.14);
-chairPtr->print();
-//creating dynamic chair object with constructor
-Chair *livingChair = new Chair(3);
-livingChair->setPrices(525.25, 434.34, 252.52);
-livingChair->print();
-delete livingChair;
-livingChair = nullptr;
-//creating dynamic array of chair objects
-Chair *collection = new Chair[SIZE];
-collection[0].setLegs(4);
-collection[0].setPrices(441.41, 552.52, 663.63);
-collection[1].setLegs(4);
-collection[1].setPrices(484.84, 959.59, 868.68);
-collection[2].setLegs(4);
-collection[2].setPrices(626.26, 515.15, 757.57);
-for (int i = 0; i < SIZE; i++)
-collection[i].print();
-return 0;
-}
+    srand(time(0)); 
+    cout << fixed << setprecision(2);
 
+    // create pointer to first chair object using default constructor
+    Chair *chairPtr = new Chair;
+    chairPtr->print();
+
+    // create a Chair object using the parameter constructor
+    double pricesArray[SIZE] = {121.21, 232.32, 414.14};
+    Chair chair2(4, pricesArray);
+    chair2.print();
+
+    //create an array of Chair objects
+    const int NUM_CHAIRS = 3;
+    Chair chairs[NUM_CHAIRS]; // Default constructors used
+    for (int i = 0; i < NUM_CHAIRS; i++) {
+        chairs[i].print();
+    }
+
+    delete chairPtr;
+
+    return 0;
+}
