d203f3022337baab47ad80f4c750d8efc4ec51e1
JZwlth <wkjazy87@gmail.com>
2024-10-10 20:10:47 -0700

    update

--- main.cpp
+++ main.cpp
@@ -30,27 +30,35 @@ public:
             prices[i] = p[i];
         }
     }
- // setters and getters
-void setLegs(int l) { legs = l; }
-int getLegs() { return legs; }
-void setPrices(double p1, double p2, double p3) {
-prices[0] = p1; prices[1] = p2; prices[2] = p3;
-}
-double getAveragePrices() {
-double sum = 0;
-for (int i = 0; i < SIZE; i++)
-sum += prices[i];
-return sum / SIZE;
-}
-void print() {
-cout << "CHAIR DATA - legs: " << legs << endl;
-cout << "Price history: " ;
-for (int i = 0; i < SIZE; i++)
-cout << prices[i] << " ";
-cout << endl << "Historical avg price: " << getAveragePrices();
-cout << endl << endl;
-}
+    
+    //deallocate memory
+    ~Chair() {
+        delete[] prices;
+    }
+
+    // Setters and getters
+    void setLegs(int l) { legs = l; }
+    int getLegs() { return legs; }
+
+    void setPrices(double p1, double p2, double p3) {
+        prices[0] = p1; prices[1] = p2; prices[2] = p3;
+    }
+    double getAveragePrices() {
+        double sum = 0;
+        for (int i = 0; i < SIZE; i++)
+            sum += prices[i];
+        return sum / SIZE;
+    }
+    void print() {
+        cout << "CHAIR DATA - legs: " << legs << endl;
+        cout << "Price history: ";
+        for (int i = 0; i < SIZE; i++)
+            cout << prices[i] << " ";
+        cout << endl << "Historical avg price: " << getAveragePrices();
+        cout << endl << endl;
+    }
 };
+
 int main() {
 cout << fixed << setprecision(2);
 //creating pointer to first chair object
@@ -76,3 +84,4 @@ for (int i = 0; i < SIZE; i++)
 collection[i].print();
 return 0;
 }
+
